dcf794fc645ef421d7c24c93b0dace61d324e271
Noah Sedlik <noahsedlik@berkeley.edu>
2025-03-22 10:14:32 -0700

    add basic setup script

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+bashrc_setup() {
+	echo "Copying to ~/.bashrc, ~/.bash_aliases, ~/.bash_path"
+	cp .bashrc ~/.bashrc
+	cp .bash_aliases ~/.bash_aliases
+	cp .bash_path ~/.bash_path
+}
+
+tmux_setup() {
+	echo "Copying to ~/.bashrc, ~/.bash_aliases, ~/.bash_path"
+	cp .tmux.conf ~/.tmux.conf
+}
+
+neovim_setup() {
+	echo "TODO: Neovim setup (installation with wget, etc)"
+	echo "Copying to ~/.config/nvim"
+	cp -r Neovim-Config/* ~/.config/nvim
+}
+
+HOME="/home/noah/dotfiles"
+
+if [[ $(pwd) != $HOME ]]; then
+	echo "[setup.sh] Error: Current working directory must be run from ~/dotfiles"
+	echo $HOME
+	echo $(pwd)
+	exit
+fi
+
+bashrc_setup()
+tmux_setup()
+neovim_setup()
+
+echo "TODO: copy settings.json to WSL2"
